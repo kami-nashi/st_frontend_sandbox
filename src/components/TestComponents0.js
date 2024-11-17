9645c6b6dbd3fee1e231a0d9a04ea6d7669b8f3e
Ashley Young <asyoung@redhat.com>
2024-11-17 18:40:11 -0500

    cleaning up files that should not had been committed

--- src/components/TestComponents0.js
+++
@@ -1,8 +0,0 @@
-
-const CompText = () => {
-    return (
-        <p>I'm a different type of imported text.</p>
-    );
-  };
-    
-  export default CompText;
--- src/pages/Terms.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Terms() {
-  return <p class="alert alert-info">Hello Cruel World</p>;
-}
